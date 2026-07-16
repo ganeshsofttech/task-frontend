cd6cd907404673c703c40d075974930ff1223a30
Ganesh <moreganesh327@gmail.com>
2026-07-16 11:37:31 +0000

    Added bootstrap alignment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import './css/bootstrap.min.css';
+import './js/bootstrap.bundle.min.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
